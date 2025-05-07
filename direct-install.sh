21c997efb752914e0201c25cdfa6e707b3bf75bc
nyros-solari <secure.duckpay@gmail.com>
2025-05-07 12:09:43 +0600

    Update direct-install.sh

--- direct-install.sh
+++ direct-install.sh
@@ -1,11 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Terminal colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 APK_LIFETIME=${1:-6} 
 
